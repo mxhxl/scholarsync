7b37b46031cd9ee42cacfc132aceaf5f21324a93
Mohamed Nasir <Mohamednasir.s2006@gmail.com>
2026-04-11 12:16:03 +0530

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
+
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${BACKEND_URL}/v1/:path*`,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
